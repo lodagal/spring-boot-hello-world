2b0b279adff4f0b2a02de303f6e8052afb627bc7
lodgal <lodagaladurgasravani2002@gmail.com>
2023-11-24 14:55:58 +0530

    updated

--- src/main/java/com/example/helloworld/controller/HelloWorldController.java
+++ src/main/java/com/example/helloworld/controller/HelloWorldController.java
@@ -6,8 +6,20 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloWorldController {
 
-    @GetMapping("/hello sravani")
-    public String sendGreetings sravani() {
-        return "Hello, sravani";
+    @GetMapping("/hello")
+    public String sendGreetings() {
+        return "Hello, World!";
+    }
+    @GetMapping("/hello2")
+    public String sendGreetings2() {
+        return "Hello, World!2";
+    }
+    @GetMapping("/hello3")
+    public String sendGreetings3() {
+        return "Hello, World!3";
+    }
+     @GetMapping("/hello4")
+    public String sendGreetings4() {
+        return "Hello, World!4";
     }
 }
